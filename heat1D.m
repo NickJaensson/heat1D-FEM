89b41dd1d5b3cc163627044c66d2ed7b2aeab251
Nick Jaensson <N.O.Jaensson@tue.nl>
2021-11-12 13:54:31 +0100

    Add Neumann and Robin BCs

--- heat1D.m
+++ heat1D.m
@@ -4,17 +4,22 @@ close all; clear
 % comparison to analytical solution (a series until infinity)
 
 % problem parameters
-ne = 50;          % number of elements
-alpha = 1;        % diffusion coefcient
-L = 0.1;          % length of the domain
-deltat = 1e-5;    % time step size
-nstep = 100;      % number of time steps
-plotevery = 10;   % plot every ... time step
-Twall = 0.1;      % wall temperature
-T0    = 1;        % initial temperature
-nplot = 200;      % number of points for plotting analytical solution
-nseries = 100;    % number of terms in analytical solution
-keepK = 1;        % 1: keep elem matrix between time steps  0: do not keep
+ne = 50;           % number of elements
+alpha = 1;         % thermal diffusivity coefcient
+L = 0.1;           % length of the domain
+deltat = 1e-5;     % time step size
+nstep = 100;       % number of time steps
+plotevery = 10;    % plot every ... time step
+nplot = 200;       % number of points for plotting analytical solution
+nseries = 100;     % number of terms in analytical solution
+keepK = 1;         % 1: keep elem matrix between time steps  0: do not keep
+BCs = [1 1];       % BCs(1) for left and BCs(2) right side, with values:
+                   % 0: Dirichlet, 1: Neumann, 2: Robin
+T0    = 1;         % initial temperature
+Twall = [0.1 0.1]; % wall temperature in case BCs = 0
+flux = [1 5];      % heat flux / (rho * cp) in case BCs = 1
+hheat = [10 5];    % heat transfer coeff. / (rho * cp) in case BCs = 2
+Tinf = [0 0];      % temperature at inf. in case BCs = 2
 
 % derived parameters
 h = L/ne;       % element size
@@ -37,8 +42,6 @@ gradxiNe = [-1/2 -1/2; 1/2 1/2];
 
 % define the initial solution of the temperature
 sol = zeros(ne+1,1)+T0;
-sol(1) = Twall;
-sol(end) = Twall;
 
 % plot the solution
 figure(1); hold on
@@ -110,35 +113,74 @@ for step = 1:nstep
         f(ie:ie+1) = f(ie:ie+1) + fe;  
         
     end
+    
+    % add Neumann boundary conditions
 
-    % add boundary condition on the left and right boundary
-    % NOTE: see userguide of eztfem for details on this approach
-
-    % indices of essential BCs
-    iess = [1 ne+1];
-
-    % indices of "free" dofs
-    iu = 2:ne;
-
-    % make a system vector with the essential BCs
-    uess = zeros(ne+1,1);
-    uess(iess) = Twall;
-
-    if ( step == 1 && keepK ) || ~keepK
-
-      % extract Kup
-      Kup = K(iu,iess);
-
-      % modify K
-      K(iu,iess) = 0;                     % Kup = 0
-      K(iess,iu) = 0;                     % Kpu = 0
-      K(iess,iess) = eye(length(iess));   % Kpp = I
-
+    if ( BCs(1) == 1 )            
+      f(1) = f(1) - deltat*flux(1);
     end
 
-    % modify f
-    f(iu) = f(iu) - Kup * uess(iess) ; % fu = fu - Kup * up
-    f(iess) = uess(iess) ;             % fp = up
+    if ( BCs(2) == 1 )    
+      f(end) = f(end) - deltat*flux(2);
+    end
+    
+    % add Robin boundary conditions
+
+    if ( BCs(1) == 2 )   
+      if ( step == 1 && keepK ) || ~keepK
+        K(1,1) = K(1,1) + deltat*hheat(1);
+      end
+      f(1) = f(1) + deltat*hheat(1)*Tinf(1);
+    end
+
+    if ( BCs(2) == 2 ) 
+      if ( step == 1 && keepK ) || ~keepK
+        K(end,end) = K(end,end) + deltat*hheat(2);
+      end        
+      f(end) = f(end) + deltat*hheat(2)*Tinf(2);
+    end    
+            
+    % add essential boundary conditions
+    % NOTE: see userguide of eztfem for details on this approach
+
+    if any ( BCs == 0 )
+        
+        % make a system vector with the essential BCs
+        uess = zeros(ne+1,1);    
+        
+        % indices of essential BCs and indices of "free" dofs
+        if BCs(1) == 0 && BCs(2) ~= 0
+          iess = [1]; 
+          iu = 2:ne+1;
+          uess(iess(1)) = Twall(1);
+        elseif BCs(1) ~= 0 && BCs(2) == 0
+          iess = [ne+1]; 
+          iu = 1:ne;
+          uess(iess(1)) = Twall(2);
+        else
+          iess = [1 ne+1]; 
+          iu = 2:ne;
+          uess([iess(1),iess(2)]) = Twall;
+        end        
+
+
+        if ( step == 1 && keepK ) || ~keepK
+
+          % extract Kup
+          Kup = K(iu,iess);
+
+          % modify K
+          K(iu,iess) = 0;                     % Kup = 0
+          K(iess,iu) = 0;                     % Kpu = 0
+          K(iess,iess) = eye(length(iess));   % Kpp = I
+
+        end
+
+        % modify f
+        f(iu) = f(iu) - Kup * uess(iess) ; % fu = fu - Kup * up
+        f(iess) = uess(iess) ;             % fp = up
+    
+    end
 
     % solve the linear system
     sol = K\f;
@@ -180,4 +222,7 @@ xlabel('$x$','Interpreter','latex')
 ylabel('$T$','Interpreter','latex')
 ax = gca; 
 ax.FontSize = 24;
-ylim([min([Twall T0]) max([Twall T0])]);
+%ylim([min([Twall T0]) max([Twall T0])]);
+
+disp(['outward flux at left boundary = ',num2str((sol(2) - sol(1)) / h)])
+disp(['outward flux at right boundary = ',num2str(-(sol(end) - sol(end-1)) / h)])
